160a5d5cf0d1cbbf37d112ea622b8e612c9cdf21
Abdoelaziz Gamal <abdoelaziz.gamal010@gmail.com>
2023-01-05 12:10:48 +0200

    Requests

--- src/Requests.js
+++ src/Requests.js
@@ -24,24 +24,16 @@ const Requests = {
   fetchActor: (id) => `/person/${id}?api_key=${API_KEY}`,
   fetchActorFilms: (cast_id) =>
     `/discover/movie?with_cast=${cast_id}&api_key=${API_KEY}`,
-  // fetchActorTvShows: (cast_id) =>
-  //   `/discover/tv?with_cast=${cast_id}&api_key=${API_KEY}`,
   fetchActorTvShows: (cast_id) =>
     `/person/${cast_id}/tv_credits?api_key=${API_KEY}&language=en-US`,
-  // fetchActorTvShows: (cast_id) =>
-  //   `/tv/${cast_id}/credits?api_key=${API_KEY}&language=en-US`,
-
   fetchFilmRecommenditin: (film_id) =>
     `/movie/${film_id}/recommendations?api_key=${API_KEY}`,
   fetchTvRecommenditin: (tv_id) =>
     `/tv/${tv_id}/recommendations?api_key=${API_KEY}`,
-  fetchGenersList: `/genre/movie/list?api_key=${API_KEY}&language=en-US
-  `,
+  fetchGenersList: `/genre/movie/list?api_key=${API_KEY}&language=en-US`,
   fetchGenersTvList: `/genre/tv/list?api_key=${API_KEY}&language=en-US`,
-
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
-  // fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
